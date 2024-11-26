9ca2fb30aae4859fe3a63214c42c8f45553bc95b
Vladislav8653 <arefin.vlad@gmail.com>
2024-11-26 13:22:01 +0300

    repository method for filtering events is ready

---
+++ DataLayer/Models/EventFilterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BusinessLayer.DtoModels.EventsDto;
+
+public class EventFilterDto
+{
+    public DateTime? Date { get; set; }
+    public string? Place { get; set; } 
+    public string? Category { get; set; } 
+}
