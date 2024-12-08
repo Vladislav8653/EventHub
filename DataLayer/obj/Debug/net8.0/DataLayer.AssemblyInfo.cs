0c1705c6d42b3b9637cfd01c594108385ffe4397
Vladislav8653 <arefin.vlad@gmail.com>
2024-12-08 23:12:48 +0300

    minor fixes

--- DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
+++ DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe94119a99704da622e9f6ff5104f8ba46b1d6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d4b71f2d49665f592596f0c4da7806b1bbbfc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
