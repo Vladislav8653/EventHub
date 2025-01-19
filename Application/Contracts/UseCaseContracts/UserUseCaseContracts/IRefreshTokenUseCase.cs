24297dce2d4bdf939a6aa518bcb57f58fa4c64e0
vlad <arefin.vlad@gmail.com>
2025-01-20 02:35:10 +0300

    created RefreshTokenUseCase.cs

---
+++ Application/Contracts/UseCaseContracts/UserUseCaseContracts/IRefreshTokenUseCase.cs
@@ -0,0 +1,8 @@
+using System.Security.Claims;
+
+namespace Application.Contracts.UseCaseContracts.UserUseCaseContracts;
+
+public interface IRefreshTokenUseCase
+{
+    Task<string> Handle(string? accessToken, string? refreshToken);
+}
---
+++ Application/UseCases/UserUseCases/RefreshTokenUseCase.cs
@@ -0,0 +1,41 @@
+using System.Security.Claims;
+using Application.Contracts.AuthContracts;
+using Application.Contracts.RepositoryContracts;
+using Application.Contracts.UseCaseContracts.UserUseCaseContracts;
+using Application.Exceptions;
+
+namespace Application.UseCases.UserUseCases;
+
+public class RefreshTokenUseCase : IRefreshTokenUseCase
+{
+    private readonly IJwtProvider _jwtProvider;
+    private readonly IRepositoriesManager _repositoriesManager;
+    public RefreshTokenUseCase(IJwtProvider jwtProvider, IRepositoriesManager repositoriesManager)
+    {
+        _jwtProvider = jwtProvider;
+        _repositoriesManager = repositoriesManager;
+    }
+
+    public async Task<string> Handle(string? accessToken, string? refreshToken)
+    {
+        if (string.IsNullOrEmpty(refreshToken))
+        {
+            throw new ArgumentNullException(nameof(refreshToken), "User does not have refresh token");
+        }
+        if (string.IsNullOrEmpty(accessToken))
+        {
+            throw new ArgumentNullException(nameof(accessToken), "User does not have an access token");
+        }
+        var claims = _jwtProvider.GetClaimsAccessToken(accessToken);
+        var userId = claims.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (string.IsNullOrEmpty(userId))
+            throw new UnauthorizedAccessException("Invalid token, can't extract user ID.");
+        if (Guid.TryParse(userId, out var userIdGuid))
+            throw new InvalidDataTypeException("Can't parse user ID to type Guid.");
+        var user = await _repositoriesManager.Users.GetUserByIdAsync(userIdGuid);
+        if (user == null)
+            throw new EntityNotFoundException("User", "id", userId);
+        _jwtProvider.ValidateRefreshToken(user.RefreshToken, refreshToken, user.RefreshTokenExpireTime);
+        return _jwtProvider.GenerateAccessToken(user);
+    }
+}
